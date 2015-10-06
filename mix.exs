ad44d177bbe79883cc5c1c41ec8c59dcc0305f56
Jonathan D. Storm <jonathan.dale.storm@protonmail.ch>
2015-10-06 10:51:34 -0500

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule NetSNMP.Mixfile do
   defp deps do
     [
       {:amrita, "~>0.4", git: "https://github.com/josephwilk/amrita.git"},
-      {:meck, git: "https://github.com/eproxus/meck.git", branch: "master"},
+      {:meck, git: "https://github.com/eproxus/meck.git", override: true},
       {:snmp_mib_ex, git: "https://github.com/jonnystorm/snmp-mib-elixir"},
       {:pathname_ex, git: "https://github.com/jonnystorm/pathname-elixir"}
     ]
